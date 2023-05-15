1549196a0984015b254fe61d2ce2e748907a50ab
Kazae67 <Kazae.zaeka@gmail.com>
2023-05-15 10:52:43 +0200

    petite correction

--- Joueur.php
+++ Joueur.php
@@ -4,15 +4,15 @@ class Player
     private $nom;
     private $prenom;
     private $sexe;
-    private $birthday;
+    private $date_naissance;
     private $nationalite;
     private $equipe;
 
-    public function __construct($nom, $prenom, $sexe, $birthday, $nationalite, $equipe){
+    public function __construct($nom, $prenom, $sexe, $date_naissance, $nationalite, $equipe){
         $this->nom= $nom;
         $this->prenom= $prenom;
         $this->sexe = $sexe;
-        $this->birthday = $birthday;
+        $this->date_naissance = $date_naissance;
         $this->nationalite = $nationalite;
         $this->equipe = $equipe;
     }
