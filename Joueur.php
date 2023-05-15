a31572a97a788492b278167e201bda55aaddeb4c
Kazae67 <Kazae.zaeka@gmail.com>
2023-05-15 11:02:20 +0200

    Ajout club

--- Joueur.php
+++ Joueur.php
@@ -6,14 +6,14 @@ class Player
     private $sexe;
     private $date_naissance;
     private $nationalite;
-    private $equipe;
+    private $clubs;
 
-    public function __construct($nom, $prenom, $sexe, $date_naissance, $nationalite, $equipe){
+    public function __construct($nom, $prenom, $sexe, $date_naissance, $nationalite){
         $this->nom= $nom;
         $this->prenom= $prenom;
         $this->sexe = $sexe;
         $this->date_naissance = $date_naissance;
         $this->nationalite = $nationalite;
-        $this->equipe = $equipe;
+        $this->clubs = [];
     }
 }
