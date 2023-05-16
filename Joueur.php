23ac5839374a6981c3797ec8049d6a9254ca77fb
Kazae67 <Kazae.zaeka@gmail.com>
2023-05-16 10:20:16 +0200

    Mise en forme majuscule

--- Joueur.php
+++ Joueur.php
@@ -41,7 +41,7 @@ class Joueur
     // GET INFO
     public function getInfo()
     {
-        $result = $this . " <br> " . $this->nationalite . " " . "(" . $this->getDate_Naissance() . " ans) :<br>";
+        $result = "<b>".strtoupper($this) . "</b><br> " . $this->nationalite . " " . "(" . $this->getDate_Naissance() . " ans) :<br>";
         foreach ($this->clubs as $club) {
             $result .= $club . "<br>";
         }
