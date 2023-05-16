8f9267ae20babe7e03621a4d6cd6709e205da21d
Kazae67 <Kazae.zaeka@gmail.com>
2023-05-16 17:44:39 +0200

    Tâche mises à jour avant la pause, reprise ce soir

--- Tache.php
+++ Tache.php
@@ -1,2 +1,4 @@
 <?php
-// mettre les getters de tout les attributs de toutes les classes
+// afficher l'année club dans carrière Ex : Killian mbape français 24 ans PSG (2017)
+// faire encore quelques essais avec les objets
+// commencer la partie CSS pour le visuel
